6ae057b438d20dac8035017888ed69e3b917701d
mari <marichu_anne@hotmail.ca>
2021-07-11 12:59:39 -0400

    edited url, views, etc

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Item, OrdersItem, Order
+from .models import Item, OrdersItem, Order
 
 admin.site.register(Item)
 admin.site.register(OrdersItem)
